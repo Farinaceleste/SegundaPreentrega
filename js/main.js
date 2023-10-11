cabf4cb4a0ab84578926d999b27627e1eb74b49f
farinaceleste <farinaceleste@gmail.com>
2023-10-11 19:30:19 -0300

    mejoré funciones

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-// funcion constructora 
-
 const Producto = function (nombre, precio) {
     this.nombre = nombre;
     this.precio = precio;
 }
 
-// array
-
-const Productos = [
+const productos = [
     { nombre: " guitarras aerodyne special telecaster", precio: 210000},
     { nombre: "guitarras american professional jazzmaster", precio: 165000},
     { nombre: "bajos jaguar bass edicion limitada", precio: 420000},
@@ -16,31 +12,22 @@ const Productos = [
     { nombre: "bajos american bass", precio: 190000},
     { nombre: "guitarras american mustang", precio: 550000},
     { nombre: "guitarras american stratocaster", precio: 450000},
-
 ]
 
-function filtroBajos() {
-    const Bajos = Productos.filter((bajos) => bajos.nombre.includes("bajos"))
-    console.log(Bajos);
-}
+function filtros() {
+    let pregunta = prompt("¿Quiere buscar bajos o guitarras?").toUpperCase()
+    const respuesta = productos.filter((instrumento) => instrumento.nombre.toUpperCase().includes(pregunta))
+    console.log(respuesta);
+}   
 
-function filtroGuitarras() {
-    const Guitarras = Productos.filter((guitarras) => guitarras.nombre.includes("guitarras"))
-    console.log(Guitarras);
-}
+let quiereaplicarFiltro = prompt("¿Quiere aplicar un filtro a su búsqueda?").toUpperCase()
 
-let quiereaplicarFiltro = prompt("¿Quiere aplicar un filtro a su búsqueda?")
-
-if (quiereaplicarFiltro === "si") {
-    let pregunta = prompt("¿Quiere buscar bajos o guitarras?")
-
-    if (pregunta === "bajos") {
-        filtroBajos();
-    } else if (pregunta === "guitarras"){
-        filtroGuitarras();
+if (quiereaplicarFiltro === "SI") {
+    filtros()
+    } else {
+        console.log(productos);
     }
 
-} else {
-    console.log(Productos);
-}
+
+
 
